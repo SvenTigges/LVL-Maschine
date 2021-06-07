4fb21d50f283d3203c25cee566b71177926bcb72
Your Name <sven.tigges@web.de>
2021-06-07 10:43:18 +0200

    add play++

--- Level_1.java
+++ Level_1.java
@@ -3,4 +3,10 @@ public class Level_1 extends Level_2 {
     private int points = 100;
     private String levelName = "Level 1";
 
+    public void play() {
+        
+        System.out.println("Hi " + levelName + "here and I have" + points + " points!");
+
+    }
+
 }
