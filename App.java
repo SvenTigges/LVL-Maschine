9b8887e8873989c2f32879a39970830e2434a26c
Your Name <sven.tigges@web.de>
2021-06-07 10:39:16 +0200

    Level ++ / Points

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App{
+
+    public public static void main(String[] args) {
+      //todo   
+    }
+}
---
+++ Level_1.java
@@ -0,0 +1,6 @@
+public class Level_1 extends Level_2 {
+    
+    private int points = 100;
+    private String levelName = "Level 1";
+
+}
